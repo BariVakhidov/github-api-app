aa2fc5db248b2e037b7fbc9bbd3c6a508e03b70d
barivakhidov <bari.vakhidov@nordclan.com>
2021-08-18 01:14:57 +0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { instance } from './client';
 
-const App = () => {
-  useEffect(() => {
-    instance.get('/users');
-  }, []);
-
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -30,5 +21,6 @@ const App = () => {
       </header>
     </div>
   );
-};
+}
+
 export default App;
