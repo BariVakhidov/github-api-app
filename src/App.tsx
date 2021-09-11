c23744f85803e7dd89c438eebe6ffa2e19e8e125
BariVakhidov <memeshuntergod@gmail.com>
2021-09-12 01:03:30 +0300

    libs, axios client, eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { instance } from './client';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    instance.get('/users');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,6 +30,5 @@ function App() {
       </header>
     </div>
   );
-}
-
+};
 export default App;
